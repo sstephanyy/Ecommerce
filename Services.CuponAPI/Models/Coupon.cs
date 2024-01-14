796b3049786438c6a387831b09bea026bcb6fa0a
Stephanyy <Stephanyy@DESKTOP-KVJF7PV>
2024-01-14 18:49:03 -0300

    Added a CouponDto to to transfer data and Coupon class

---
+++ Services.CuponAPI/Models/Coupon.cs
@@ -0,0 +1,13 @@
+﻿namespace Services.CuponAPI.Models
+{
+    public class Coupon
+    {
+        public int CouponId { get; set; }
+
+        public string CouponCode { get; set; }
+
+        public double DiscontAmount { get; set; } //how much discount will someone get
+
+        public int MinAmount { get; set; }
+    }
+}
---
+++ Services.CuponAPI/Models/Dto/CouponDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Services.CuponAPI.Models.Dto
+{
+    // DTO -> The primary purpose of a DTO is to encapsulate and transport data across different layers of an application, providing a way to transfer structured data without exposing the internal details of the underlying system
+    // when sending this data to the frontend, the CouponDto IS USED to encapsulate only the necessary information and structure
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+
+        public string CouponCode { get; set; }
+
+        public double DiscontAmount { get; set; } //how much discount will someone get
+
+        public int MinAmount { get; set; }
+    }
+}
